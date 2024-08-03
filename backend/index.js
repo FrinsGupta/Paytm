eaa799852745bcd58f79673b5a945c347237e001
FrinsGupta <pryncegupta@gmail.com>
2024-08-04 00:12:29 +0530

    Added cors

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,14 @@ const cors = require("cors");
 
 const port = process.env.PORT
 
-app.use(cors())
+const corsOptions = {
+    origin: 'https://e-wallet-app-three.vercel.app',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true, // if you need to handle cookies or HTTP authentication
+  };
+
+app.use(cors(corsOptions))
 
 app.use(express.json());
 app.use(bodyParser.json());
